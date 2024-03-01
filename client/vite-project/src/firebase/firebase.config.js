7166c75106f0695aa67d8d7d09628b148380b5fb
Prayag rajwade <85736916+Prayagrajwade@users.noreply.github.com>
2024-03-01 10:16:53 +0530

    Update firebase.config.js
    Api detail's Changed

--- client/vite-project/src/firebase/firebase.config.js
+++ client/vite-project/src/firebase/firebase.config.js
@@ -4,14 +4,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCdn5OuAlYMZYF4MValP83T13tgMxw4rcw",
-  authDomain: "mern-book-inventory-dd9bf.firebaseapp.com",
-  projectId: "mern-book-inventory-dd9bf",
-  storageBucket: "mern-book-inventory-dd9bf.appspot.com",
-  messagingSenderId: "648379376866",
-  appId: "1:648379376866:web:2c28c377c90faf9852aabb"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
